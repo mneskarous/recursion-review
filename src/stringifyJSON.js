9a83384429006d73ab25419190d149dbc9dff798
Michael Eskarous <mneskarous@gmail.com>
2019-07-15 12:00:33 -0700

    Fixed unit tests and stringified null and number

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,7 +7,13 @@ var stringifyJSON = function(obj) {
   // your code goes here
   // var arr = [1, {a: 1}, 3, 4, 5,];
   // null
+  if (obj === null) {
+    return 'null';
+  }
   // number
+  if (typeof obj === 'number') {
+    return '"' + obj + '"';
+  }
   // string
   // boolean
   // array
